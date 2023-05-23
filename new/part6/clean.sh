543a88af04b14dd8b656fe1a704df9152cb271e4
julieanb <julieanb@kali-host.kali-domain>
2023-05-23 19:43:34 -0400

    lastpart

--- new/part6/clean.sh
+++ new/part6/clean.sh
@@ -3,7 +3,7 @@
 sudo docker stop part6_server_1
 sudo docker stop part6_proxy_1
 sudo docker rm part6_server_1
-sudo docker rm art6_proxy_1
+sudo docker rm part6_proxy_1
 sudo docker rmi part6_proxy -f
 sudo docker rmi part6_server -f
 sudo docker rmi alpine -f
